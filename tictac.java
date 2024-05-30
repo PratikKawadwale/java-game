45296909d2fe239463197ba7ae60e0223f7bba4a
pratik kawadwale <142656318+PratikKawadwale@users.noreply.github.com>
2024-05-30 08:13:56 +0530

    Create tictac.java

---
+++ tictac.java
@@ -0,0 +1,14 @@
+class main
+{
+   public static void main(String[] args)
+   {
+     char[][] board=new char[3][3];
+     for(int r=0;r<board.length; r++)
+     {
+       for(int c=0;c<board[r].length;c++)
+       {
+         board[r][c]=' ';
+       }
+      }
+   }
+}
